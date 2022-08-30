ba47955e92a844a4cc574662b925012c57bc4af5
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-08-29 21:12:58 -0300

    atualiza a conexão com o banco de dados

--- src/database/index.js
+++ src/database/index.js
@@ -1,18 +1,14 @@
 import { Sequelize } from "sequelize";
-
-import TrainerModel from "../app/models/trainer/TrainerModel";
-
 import databaseConfig from "../config/database";
 
-const models = [TrainerModel];
-
-export default class Database {
+class Database {
   constructor() {
     this.init();
   }
+
   init() {
     this.connection = new Sequelize(databaseConfig);
-
-    models.map((model) => model.init(this.connection));
   }
 }
+
+export default new Database().connection;
